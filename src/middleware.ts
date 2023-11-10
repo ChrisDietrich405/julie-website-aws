bf9bb3701d3fe8e8354a1b843eb8acadfb603d6e
Chris <chrisdietrich3666@gmail.com>
2023-11-09 20:23:06 -0500

    added middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,47 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Params } from "./app/types/params";
 
-export function middleware(request:NextRequest, {params}: Params) {
-    console.log("middleware", request.url)
-    return NextResponse.next()
+// import { Params } from "./app/types/params";
+
+// interface IAuth extends Headers {}
+
+// export function middleware(req: NextRequest, { params }: Params) {
+
+//   let token: IAuth = req.headers;
+
+//   let anothername = token.values;
+//   console.log("anothername", anothername);
+//   // let tokenHeaders: IAuthorization = new Headers(req.headers.authorization);
+
+//   //   tokenHeaders: IHeadersAuthorization = tokenHeaders.authorization?.split(" ")[1]
+
+//   // if(!token) {
+//   //     NextResponse.json({status: 401, message: "Unauthorized"})
+//   // }
+
+//   return NextResponse.next();
+// }
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import jwt, { JwtPayload } from "jsonwebtoken"
+
+export function middleware(req: NextRequest, res: NextResponse) {
+  const headersInstance = headers();
+  let authorization = headersInstance.get("authorization");
+
+  // if(!authorization) {
+  //   return res.status(401).json({})
+  // }
+
+  const tokenNumber = authorization?.split(" ")[1];
+
+  const decodedToken = jwt.verify(tokenNumber, process.env.JWT_SECRET as string)
+
+
+  console.log("auth", authorization);
+  
+
+
 }
 
-
-
 export const config = {
-    matcher: '/api/user/:path*',
-  }
+  matcher: "/api/user/:path*",
+};
