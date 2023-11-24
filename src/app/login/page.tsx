592d3a83f96b427edd967fb47f7d22f9749a9ff3
Chris <chrisdietrich3666@gmail.com>
2023-11-24 11:16:56 -0500

    added loader

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,20 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
+import { Oval } from "react-loader-spinner";
+
+<Oval
+  height={80}
+  width={80}
+  color="#4fa94d"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+  ariaLabel="oval-loading"
+  secondaryColor="#4fa94d"
+  strokeWidth={2}
+  strokeWidthSecondary={2}
+/>;
 
 import { userContext } from "../context/userContext";
 
@@ -13,6 +27,7 @@ import "react-toastify/dist/ReactToastify.css";
 const Login = () => {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const { userId, setUserId } = useContext(userContext);
 
@@ -20,16 +35,16 @@ const Login = () => {
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await axios.post("http://localhost:3000/api/auth", {
         email,
         password,
       });
 
-      setUserId(response.data.userId)
+      setUserId(response.data.userId);
 
       localStorage.setItem("token", `Bearer ${response.data.token}`);
-
 
       // Router.push("/available-works");
 
@@ -40,6 +55,7 @@ const Login = () => {
       //     Router.push("/useraccount");
       //   }
       router.push("/");
+      setLoading(false);
     } catch (error) {
       toast.error("Incorrect email or password");
     }
@@ -48,38 +64,45 @@ const Login = () => {
   return (
     <>
       <ToastContainer />
-      <form onSubmit={onSubmit} className={styles.form}>
-        <h2>Log in</h2>
-        <label htmlFor="email" className={styles.label}>
-          email
-          <input
-            type="text"
-            name="email"
-            id="email"
-            className={styles.input}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label htmlFor="password" className={styles.label}>
-          password
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className={styles.input}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
 
-        <button type="submit" className={styles.button}>
-          Submit
-        </button>
+      {loading ? (
+        <div className={styles.loader}>
+          <Oval />
+        </div>
+      ) : (
+        <form onSubmit={onSubmit} className={styles.form}>
+          <h2>Log in</h2>
+          <label htmlFor="email" className={styles.label}>
+            email
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className={styles.input}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor="password" className={styles.label}>
+            password
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className={styles.input}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
 
-        <p style={{ marginBottom: "10px" }}>New to Julie Dtrick?</p>
-        <Link href="/create-account">Create an account</Link>
-      </form>
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
+
+          <p style={{ marginBottom: "10px" }}>New to Julie Dtrick?</p>
+          <Link href="/create-account">Create an account</Link>
+        </form>
+      )}
     </>
   );
 };
