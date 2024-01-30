1b8b9c8a90007fe8476b7fe833c7bd97970e64f8
Flávio Severiano <severiano.flavio18@gmail.com>
2024-01-30 18:30:35 -0300

    refactor(payment): adjust payment details page

--- src/app/delivery-details/page.tsx
+++ src/app/delivery-details/page.tsx
@@ -1,115 +1,50 @@
 "use client";
-import React, { useState, FormEvent } from "react";
-import axios from "axios";
-
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
-
-import { LoadingButton } from '@mui/lab';
+import React, {useState, FormEvent, useEffect} from "react";
+import {CardElement, Elements, useElements} from '@stripe/react-stripe-js';
 
 import {
-  Box,
-  Button,
+  Box, CircularProgress,
   Container,
-  Grid,
-  Stack,
-  Step,
-  StepLabel,
-  Stepper,
   Typography
 } from "@mui/material";
-import IdentificationForm from "@/components/forms/IdentificationForm";
-import AddressForm from "@/components/forms/AddressForm";
-import PaymentForm from "@/components/forms/PaymentForm";
-import {StepFormData} from "./page.types";
-import { OrdersApi } from "@/services/orders.service";
 
-const steps = ['Identification', 'Address', 'Payment'];
+import {loadStripe} from '@stripe/stripe-js';
+
+import {StripeApi} from "@/services/stripe.service";
+import CheckoutForm from "@/components/forms/CheckoutForm/CheckoutForm";
+
+const stripePromise = loadStripe('pk_test_51OdBniCc07zP9ZGY7G60C6fNxPqhjEPprGycArVZocmuXpq27xjZ172MxygSUhQiwrxEGIuXWsths2DCNnSZgnvP00brlkMDp2');
+
 
 const CreateAccount = () => {
-  const [formData, setFormData] = useState<StepFormData>({
-    identification: {
-      firstName: '',
-      lastName: '',
-      email: ''
-    },
-    deliveryAddress: {
-      streetAddress: '',
-      city: '',
-      postalCode: '',
-      state: ''
-    },
-    payment: {
-      cardNumber: '',
-      cardHolderName: '',
-      expiryDate: '',
-      cvv: ''
-    },
-  });
+const [ clientSecret, setClientSecret ] = useState('');
 
-  const [ loader, setLoader] = useState(false);
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret,
+  }
 
-  const router = useRouter();
+  useEffect(() => {
+    if (!clientSecret) {
 
-  const onSubmit = async (e?: FormEvent<HTMLFormElement>) => {
-    e?.preventDefault();
+    (async function ()  {
 
-    try {
-      setLoader(true);
-      const response = await OrdersApi.post({
-        customer: formData.identification,
-        deliveryAddress: formData.deliveryAddress,
-      });
+    const params = new URLSearchParams();
+    params.append('amount', '200');
+    params.append('currency', 'usd');
+    const response = await StripeApi.createPaymentIntent(params);
 
-      localStorage.setItem("orderCode", response.data.orderCode);
-      router.push("/payment");
-
-    } catch (error) {
-      toast.error("Incorrect email or password");
-    } finally {
-      setLoader(false);
+    setClientSecret(response.data.client_secret)
+    })()
     }
-  };
-
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set<number>());
-
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    if (activeStep === steps.length - 1) {
-      return onSubmit();
-    }
+  }, []);
 
 
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const getStepContent = (step: number) => {
-    switch (step) {
-      case 0:
-        return <IdentificationForm formData={formData.identification} setFormData={setFormData} />;
-      case 1:
-        return <AddressForm formData={formData.deliveryAddress} setFormData={setFormData} />;
-      case 2:
-        return <PaymentForm formData={formData.payment} setFormData={setFormData} />;
-    }
-  };
-
-  return (
+      return (
+        !clientSecret ?
+          <CircularProgress />
+          :
+    <Elements stripe={stripePromise} options={options}>
     <Container maxWidth="xl" sx={{
       paddingY: 5
     }}>
@@ -127,44 +62,11 @@ const CreateAccount = () => {
             paddingY: 3
           }}
         >Fill the information bellow</Typography>
-
-        <Stack>
-          <Stepper alternativeLabel activeStep={activeStep}>
-            {steps.map((label) => {
-              return (
-                <Step key={label}>
-                  <StepLabel>
-                    {label}
-                  </StepLabel>
-                </Step>
-
-              );
-            })}
-          </Stepper>
-
-          <Box
-            maxWidth={600}
-            marginX="auto"
-            marginTop={10}
-          >
-            {getStepContent(activeStep)}
-            <Grid container spacing={2} marginTop={2}>
-              <Grid item>
-                <Button disabled={activeStep === 0} onClick={handleBack}>
-                  Back
-                </Button>
-              </Grid>
-              <Grid item>
-                <LoadingButton variant="contained" color="primary" onClick={handleNext}>
-                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                </LoadingButton>
-              </Grid>
-            </Grid>
-          </Box>
-        </Stack>
+        <CheckoutForm clientSecret={clientSecret} />
       </Box>
     </Container>
-  );
+    </Elements>
+      )
 };
 
 export default CreateAccount;
